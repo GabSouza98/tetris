a518a577f8c735b306f15550b7455c77e0284261
GabSouza98 <gabrielaraujodesouza98@gmail.com>
2021-12-10 20:21:43 -0300

    First Commit

--- sketch.js
+++ sketch.js
@@ -1,23 +1,41 @@
 //constants
-const canvasWidth = 500;
+const canvasWidth = 400;
 const canvasHeight = 600;
 const backgroundColor = 50;
-const boxDim = 30;
+const boxDim = 20;
 const timer = 800;
+
+const L = [ [0, 0, 0],            
+            [1, 0, 0],
+            [1, 1, 1] ];
+
+const T = [ [0, 0, 0],            
+            [0, 1, 0],
+            [1, 1, 1] ];
+
+const J = [ [0, 0, 0],            
+            [0, 0, 1],
+            [1, 1, 1] ];
 
 //variables
 let myBox;
+let myPiece;
+let speed = 10;
+let pieceCount = 0;
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
-  myBox = new Box(canvasWidth / 2, 0, boxDim, { r: 150, g: 48, b: 95 });
-  setInterval(applyGravity, timer);
+  //myBox = new Box(canvasWidth / 2, 0, boxDim, { r: 150, g: 48, b: 95 });
+  
+  myPiece = new Piece(canvasWidth/2, 0, T, color = { r: 80, g: 150, b: 20 })
+   
 }
 
 function draw() {
   background(backgroundColor);
-  drawGrid();
-  myBox.show();
+  drawGrid();  
+  myPiece.show();  
+  myPiece.update();  
 }
 
 class Box {
@@ -40,12 +58,52 @@ class Piece {
     this.x = x;
     this.y = y;
     this.shape = shape;
-    this.color = color;
+    this.color = color;  
+    this.boxes = this.createBoxes();
   }
-}
 
-function applyGravity() {
-  myBox.y += boxDim;
+  createBoxes() {
+    let boxes = []
+    let { r, g, b } = this.color;
+    fill(r, g, b);
+    for(var i=0;i<this.shape.length;i++) {
+      for(var j=0;j<this.shape.length;j++) {
+        var existBox = this.shape[j][i];
+        boxes.push(
+          new Box(this.x + i*existBox*boxDim,
+                  this.y + j*existBox*boxDim,
+                  existBox*boxDim,
+                  this.color)
+        );        
+      }
+    }
+    return boxes;
+  }
+
+  show() {
+    let { r, g, b } = this.color;
+    fill(r, g, b);
+    for(var i=0;i<this.boxes.length;i++) {   
+      this.boxes[i].show();
+    }    
+  }
+
+  applyGravity() {
+    for(var i=0;i<this.boxes.length;i++) {   
+      this.boxes[i].y += boxDim;      
+    }   
+  }
+
+  update() {
+    
+       
+    
+
+    if(frameCount % speed == 0 ) {
+      this.applyGravity();
+    }
+  }
+
 }
 
 function drawGrid() {
