78502d6bdfa0d33af2e50fdeee7f48f3992981c4
mateus trentz <mateus.trentz@gmail.com>
2021-12-12 01:14:58 -0300

    minha versao de gravidade

--- sketch.js
+++ sketch.js
@@ -1,198 +1,219 @@
 //constants
 const rows = 18;
 const cols = 10;
-const ratio = rows/cols;
+const ratio = rows / cols;
 const canvasWidth = 300;
-const canvasHeight = ratio*canvasWidth;
+const canvasHeight = ratio * canvasWidth;
 const backgroundColor = 50;
-const speed = 20;
+const speed = 5;
 
 const pieces = {
-    1: {name: "L",
-        color: '#ff8d00',
-        matrix:[[1, 1, 1],            
-                [1, 0, 0],
-                [0, 0, 0]]
-        },
-    2: {name: "T",
-        color: '#9f0096',
-        matrix:[[1, 1, 1],            
-                [0, 1, 0],
-                [0, 0, 0]]
-        },
-    3: {name: "J",
-        color: '#ff51bc',
-        matrix: [ [1, 1, 1],            
-                  [0, 0, 1],
-                  [0, 0, 0] ]
-        },
-    4: {name: "S",
-        color: '#f60000',
-        matrix: [ [0, 1, 1],            
-                  [1, 1, 0],
-                  [0, 0, 0] ]
-        },
-    5: {name: "Z",
-        color: '#69b625',
-        matrix: [ [1, 1, 0],            
-                  [0, 1, 1],
-                  [0, 0, 0] ]
-        },
-    6: {name: "O",
-        color: '#faff00',
-        matrix: [ [1, 1],            
-                  [1, 1] ]
-        },
-    7: {name: "I",
-        color: '#00e4ff',
-        matrix: [ [1, 1, 1, 1],   
-                  [0, 0, 0, 0],       
-                  [0, 0, 0, 0],  
-                  [0, 0, 0, 0] ]
-        },
-}
-
+  1: {
+    name: "L",
+    color: "#ff8d00",
+    matrix: [
+      [1, 1, 1],
+      [1, 0, 0],
+      [0, 0, 0],
+    ],
+  },
+  2: {
+    name: "T",
+    color: "#9f0096",
+    matrix: [
+      [1, 1, 1],
+      [0, 1, 0],
+      [0, 0, 0],
+    ],
+  },
+  3: {
+    name: "J",
+    color: "#ff51bc",
+    matrix: [
+      [1, 1, 1],
+      [0, 0, 1],
+      [0, 0, 0],
+    ],
+  },
+  4: {
+    name: "S",
+    color: "#f60000",
+    matrix: [
+      [0, 1, 1],
+      [1, 1, 0],
+      [0, 0, 0],
+    ],
+  },
+  5: {
+    name: "Z",
+    color: "#69b625",
+    matrix: [
+      [1, 1, 0],
+      [0, 1, 1],
+      [0, 0, 0],
+    ],
+  },
+  6: {
+    name: "O",
+    color: "#faff00",
+    matrix: [
+      [1, 1],
+      [1, 1],
+    ],
+  },
+  7: {
+    name: "I",
+    color: "#00e4ff",
+    matrix: [
+      [1, 1, 1, 1],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
+  },
+};
 
 const timer = 800;
 let board;
 let piece;
 
 function setup() {
-    createCanvas(canvasWidth, canvasHeight);    
-    board = new Board(); 
-    piece = new Piece();  
-    //generatePiece();   
-    
-  }
-  
+  createCanvas(canvasWidth, canvasHeight);
+  board = new Board();
+  piece = new Piece();
+  //generatePiece();
+}
+
 function draw() {
-    background(backgroundColor);
-    board.drawGrid();  
-    board.draw();   
-    piece.draw();   
-    //piece.update();
+  background(backgroundColor);
+  board.drawGrid();
+  board.draw();
+  piece.draw();
+  piece.update();
 }
 
 class Piece {
-    constructor() {
-        this.pieceNumber = this.generatePieceNumber();
-        this.piece = pieces[this.pieceNumber];
-        this.board = this.startBoard();
-        this.boxHeight = canvasHeight/rows;
-        this.boxWidth = canvasWidth/cols;
-        this.boxSize = Math.floor(this.boxHeight,this.boxWidth);         
-                
+  constructor() {
+    this.pieceNumber = this.generatePieceNumber();
+    this.piece = pieces[this.pieceNumber];
+    this.board = this.startBoard();
+    this.boxHeight = canvasHeight / rows;
+    this.boxWidth = canvasWidth / cols;
+    this.boxSize = Math.floor(this.boxHeight, this.boxWidth);
+  }
+
+  generateBoard() {
+    let b = [];
+    let lineArray = [];
+    for (var i = 0; i < rows; i++) {
+      for (var j = 0; j < cols; j++) {
+        lineArray.push(0);
+      }
+      b.push(lineArray);
+      lineArray = [];
     }
+    return b;
+  }
 
-    generateBoard() {
-        let b = [];
-        let lineArray = [];
-        for(var i=0; i<rows;i++) {
-            for(var j=0; j<cols; j++) {
-                lineArray.push(0);
-            }
-        b.push(lineArray);
-        lineArray = [];
-        }
-        return b;
+  startBoard() {
+    let b = this.generateBoard();
+    let pieceNumber = this.pieceNumber;
+    let p = pieces[pieceNumber];
+
+    let halfCols = Math.floor(cols / 2);
+
+    for (var i in p.matrix) {
+      for (var j in p.matrix[i]) {
+        i = int(i);
+        j = int(j);
+        b[i][j + halfCols - 1] = pieceNumber * p.matrix[i][j];
+      }
     }
+    return b;
+  }
 
-    startBoard() {
-
-        let b = this.generateBoard();        
-        let pieceNumber = this.pieceNumber;
-        let p = pieces[pieceNumber];
-        
-        let halfCols = Math.floor(cols/2);        
-
-        for (var i in p.matrix) {            
-            for (var j in p.matrix[i]) { 
-                i = int(i);
-                j = int(j);                                            
-                b[i][j + halfCols - 1] = pieceNumber * p.matrix[i][j];                
-            }
-        }       
-        return b;
-        
+  update() {
+    if (frameCount % speed == 0) {
+      this.applyGravity();
     }
+  }
 
-    update() {
-        if(frameCount % speed == 0 ) {
-            this.applyGravity();
-            
+  applyGravity() {
+    console.log(this.board);
+    for (let i = rows - 1; i >= 0; i--) {
+      for (let j = 0; j < cols; j++) {
+        if (this.board[i][j] > 0) {
+          if (i + 1 >= rows) {
+            return;
           }
-    }
-
-    applyGravity() {
-        
-    }
-
-    draw() {
-        fill(this.piece.color)
-        for (var i in this.board) {            
-            for (var j in this.board[i]) {
-                if(this.board[i][j]>0) {                    
-                    rect(j*this.boxSize, i*this.boxSize, this.boxSize, this.boxSize);
-                }                 
-            }
+          this.board[i][j] = 0;
+          this.board[i + 1][j] = this.pieceNumber;
         }
+      }
     }
+  }
 
-    generatePieceNumber() {
-        let number = Math.floor(1 + Math.random() * (Object.keys(pieces).length))        
-        return number;
+  draw() {
+    fill(this.piece.color);
+    for (var i in this.board) {
+      for (var j in this.board[i]) {
+        if (this.board[i][j] > 0) {
+          rect(j * this.boxSize, i * this.boxSize, this.boxSize, this.boxSize);
+        }
+      }
     }
+  }
 
-
+  generatePieceNumber() {
+    let number = Math.floor(1 + Math.random() * Object.keys(pieces).length);
+    return number;
+  }
 }
 
 class Board {
-    constructor() {
-        this.board = this.createBoard();        
-        this.boxHeight = canvasHeight/rows;
-        this.boxWidth = canvasWidth/cols;
-        this.boxSize = Math.floor(this.boxHeight,this.boxWidth);        
-    }   
+  constructor() {
+    this.board = this.createBoard();
+    this.boxHeight = canvasHeight / rows;
+    this.boxWidth = canvasWidth / cols;
+    this.boxSize = Math.floor(this.boxHeight, this.boxWidth);
+  }
 
-    createBoard() {
-        let boxes = [];
-        let lineArray = [];
-        for(var i=0; i<rows;i++) {
-            for(var j=0; j<cols; j++) {
-                lineArray.push(0);
-            }
-        boxes.push(lineArray);
-        lineArray = [];
-        }
+  createBoard() {
+    let boxes = [];
+    let lineArray = [];
+    for (var i = 0; i < rows; i++) {
+      for (var j = 0; j < cols; j++) {
+        lineArray.push(0);
+      }
+      boxes.push(lineArray);
+      lineArray = [];
+    }
     return boxes;
-    }
+  }
 
-    drawGrid() {
-        //desenha linhas horizontais              
-        for (var i = 0; i <= rows; i++) {
-            stroke(20);
-            strokeWeight(0.5);
-            line(0, i*this.boxSize, canvasWidth, i*this.boxSize);
-        }
-        //desenha linhas verticais
-        for (var j = 0; j <= cols; j++) {
-            stroke(20);
-            strokeWeight(0.5);
-            line(j*this.boxSize, 0, j*this.boxSize, canvasHeight);
-        }
-        
+  drawGrid() {
+    //desenha linhas horizontais
+    for (var i = 0; i <= rows; i++) {
+      stroke(20);
+      strokeWeight(0.5);
+      line(0, i * this.boxSize, canvasWidth, i * this.boxSize);
     }
+    //desenha linhas verticais
+    for (var j = 0; j <= cols; j++) {
+      stroke(20);
+      strokeWeight(0.5);
+      line(j * this.boxSize, 0, j * this.boxSize, canvasHeight);
+    }
+  }
 
-    draw() {
-        for (var i in this.board) {            
-            for (var j in this.board[i]) {
-                if(this.board[i][j]>0) {
-                    fill(150, 140, 130);
-                    rect(i*this.boxSize, j*this.boxSize, this.boxSize, this.boxSize);
-                }  
-               
-            }
+  draw() {
+    for (var i in this.board) {
+      for (var j in this.board[i]) {
+        if (this.board[i][j] > 0) {
+          fill(150, 140, 130);
+          rect(i * this.boxSize, j * this.boxSize, this.boxSize, this.boxSize);
         }
+      }
     }
+  }
 }
-
